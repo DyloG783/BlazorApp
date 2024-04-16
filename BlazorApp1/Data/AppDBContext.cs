bef0ba1c77c8b92bb61a3670eb66be8e4500e3d7
dylandigby87 <dylan.m.c.digby@gmail.com>
2024-04-16 11:53:28 +1000

    setup entity framework with local sql server development database

---
+++ BlazorApp1/Data/AppDBContext.cs
@@ -0,0 +1,22 @@
+﻿using BlazorApp1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public IConfiguration _config { get; set; }
+        public AppDbContext(IConfiguration config) 
+        { 
+            _config = config;
+        }
+
+        // connection string currently defined in 'appsettings.json'
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DevelopmentDatabase"));
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,10 +1,12 @@
 using BlazorApp1.Components;
+using BlazorApp1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddScoped<AppDbContext>();
 
 var app = builder.Build();
 
