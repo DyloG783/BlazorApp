d9813202caf2b6f813f3622562de6fb049ef4be3
dylandigby87 <dylan.m.c.digby@gmail.com>
2024-04-17 14:01:52 +1000

    connected BUnit blazor component unit test

---
+++ BlazorApp1.Unit/CounterCSharpTests.cs
@@ -0,0 +1,35 @@
+using BlazorApp1.Components.Pages;
+
+namespace BlazorApp1.Unit
+{
+    /// <summary>
+    /// These tests are written entirely in C#.
+    /// Learn more at https://bunit.dev/docs/getting-started/writing-tests.html#creating-basic-tests-in-cs-files
+    /// </summary>
+    [TestClass]
+    public class CounterCSharpTests : BunitTestContext
+    {
+        [TestMethod]
+        public void CounterStartsAtZero()
+        {
+            // Arrange
+            var cut = RenderComponent<Counter>();
+
+            // Assert that content of the paragraph shows counter at zero
+            cut.Find("p").MarkupMatches("<p role=\"status\">Current count: 0</p>");
+        }
+
+        [TestMethod]
+        public void ClickingButtonIncrementsCounter()
+        {
+            // Arrange
+            var cut = RenderComponent<Counter>();
+
+            // Act - click button to increment counter
+            cut.Find("button").Click();
+
+            // Assert that the counter was incremented
+            cut.Find("p").MarkupMatches("<p role=\"status\">Current count: 1</p>");
+        }
+    }
+}
